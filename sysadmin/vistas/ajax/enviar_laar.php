e787e3a078cfe72eaf9d33c6fa2ae7b338faefd6
Jeimy Jara <jeimyjara@hotmail.com>
2024-05-13 20:06:32 -0500

    Mktp

--- sysadmin/vistas/ajax/enviar_laar.php
+++ sysadmin/vistas/ajax/enviar_laar.php
@@ -67,10 +67,10 @@ $row_cnt = mysqli_num_rows($query);
 if ($row_cnt > 0) {
     while ($row = mysqli_fetch_array($query)) {
         $numero_sig = $row['id_guia'] + 1;
-        $guia_sistema = 'MKT' . $numero_sig;
+        $guia_sistema = 'MKTP' . $numero_sig;
     }
 } else {
-    $guia_sistema = "MKT1";
+    $guia_sistema = "MKTP1";
 }
 if (
     isset($_SERVER['HTTPS']) &&
