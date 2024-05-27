c9e7e2e121c08fb8e3c0033b1e6ac516c91c6e54
Jeimy Jara <jeimyjara@hotmail.com>
2024-05-27 10:59:54 -0500

    xd

--- sysadmin/vistas/ajax/buscar_wallet.php
+++ sysadmin/vistas/ajax/buscar_wallet.php
@@ -33,6 +33,8 @@ if ($_GET['filtro']) {
         $band = "btn-primary";
         $bandd = "btn-secondary";
     } else {
+
+
         $band = "btn-secondary";
         $bandd = "btn-primary";
     }
