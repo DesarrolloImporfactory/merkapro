2eb627402a2ea314577365ea675e39af52367170
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-03 11:05:02 -0500

    XD

--- sysadmin/vistas/html/includes/header_start.php
+++ sysadmin/vistas/html/includes/header_start.php
@@ -39,6 +39,11 @@ require_once "../funciones.php";
 
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.3/css/all.min.css" rel="stylesheet">
 	<link href="https://cdnjs.cloudflare.com/ajax/libs/select2/4.0.13/css/select2.min.css" rel="stylesheet" />
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/flatpickr/dist/flatpickr.min.css">
+	<script src="https://cdn.jsdelivr.net/npm/flatpickr"></script>
+	<script src="https://npmcdn.com/flatpickr@4.6.13/dist/l10n/es.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://npmcdn.com/flatpickr@4.6.13/dist/themes/material_blue.css">
+
 	<style>
 		.select2-container--default .select2-selection--single {
 			height: 38px;
