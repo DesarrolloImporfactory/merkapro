c3adcaa1cfb4cacc68dbe73f7eea18b1e1631b87
Jeimy Jara <jeimyjara@hotmail.com>
2024-06-17 13:46:32 -0500

    XD

--- sysadmin/vistas/ajax/buscar_wallet.php
+++ sysadmin/vistas/ajax/buscar_wallet.php
@@ -38,8 +38,7 @@ if ($_GET['filtro']) {
         $bandd = "btn-primary";
     }
 }
-echo $dominio_actual;
-//Finaliza Control de Permisos
+
 $action = (isset($_REQUEST['action']) && $_REQUEST['action'] != NULL) ? $_REQUEST['action'] : '';
 if ($dominio_actual == 'merkapro.ec' || $dominio_actual === "77.37.67.232") {
 
@@ -75,7 +74,7 @@ if ($dominio_actual == 'merkapro.ec' || $dominio_actual === "77.37.67.232") {
         $query = mysqli_query($conexion, $sql);
         $query = mysqli_fetch_all($query);
 
-        if ($numrows > 0 && $dominio_actual == 'merkapro.ec') {
+        if ($numrows > 0 && ($dominio_actual == 'merkapro.ec' || $dominio_actual === "77.37.67.232")) {
 ?>
             <!-- activar modal -->
             <div class="">
